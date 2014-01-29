6f9c7e43be882b90c7de76057b00ac069e7653b0
root <root@localhost.localdomain>
2014-01-30 00:32:59 +0900

    attribute の項目の追加

--- metadata.rb
+++ metadata.rb
@@ -13,3 +13,23 @@ supports "centos"
 
 recipe "rstool::setup_rsdns",
   "Install and setup Rackspace DNS tool"
+
+# == Attributes
+#
+attribute "rstool",
+  :display_name => "General Rackspace tool Options",
+  :type => "hash"
+
+attribute "rstool/rackspace_username",
+  :display_name => "Rackspace username",
+  :description =>
+     "Rackspace username",
+  :required => "required",
+  :recipes => ["rstool::setup_rsdns"]
+
+attribute "rstool/rackspace_auth_key",
+  :display_name => "Rackspace Authkey",
+  :description =>
+     "Rackspace auth_key",
+  :required => "required",
+  :recipes => ["rstool::setup_rsdns"]
